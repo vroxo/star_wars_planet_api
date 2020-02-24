0f1e343c271eed93b699d07364a0cdb09a32bc63
vitorroxo <vitor@mobi2buy.com>
2020-02-23 23:58:28 -0300

    Create setup app flask

---
+++ app/app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from flask_mongoengine import MongoEngine
+
+app = Flask(__name__)
+app.config["SECRET_KEY"] = "flask+mongoengine=<3"
+app.config['MONGODB_SETTINGS'] = {
+    'db': 'star_wars_planets',
+    'username': 'skywalker',
+    'password': 'skywalker',
+    'authentication_source': 'admin'
+}
+
+db = MongoEngine(app)
+
+
+
